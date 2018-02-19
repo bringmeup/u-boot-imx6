1170b298a12fedd0cda5038dd24c77076c7e9568
Michal Stawinski <michal.stawinski@gmail.com>
2018-02-19 05:01:42 +0100

    nexo: add script to create sdcard

---
+++ burn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SDCARD=$1
+
+function umountall() {
+	sudo umount "$1"p1
+	sudo umount "$1"p2
+	sudo umount "$1"p3
+	sudo umount "$1"p4
+}
+
+#make sure it's safe to play with the disk
+umountall $SDCARD
+
+# prepare partitions -- make sure there is some space left for uboot
+sudo sfdisk $SDCARD < sdcard.layout
+sudo mkfs.vfat "$SDCARD"p1
+
+#  +-----+------+--------+-----+----------------
+#  | MBR |  ... | u-boot | ... | FAT partition ...
+#  +-----+------+--------+-----+----------------
+#  0     512    1024           20M
+
+# copy uboot (skip mbr)
+sudo dd if=u-boot.imx of=$SDCARD bs=1024 seek=1 conv=fsync
+
+# make sure it's safe to remove
+sync
+umountall $SDCARD
